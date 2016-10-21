e38800bebf89d7dbd572b4722cfe15157fe782d9
Sirplentifus <simao.marto@gmail.com>
2016-10-21 12:41:59 +0100

    Controlo dos 2 leds em simultâneo
    O controlo está com problemas - um dos leds é lento e às vezes oscila.
    Removeu-se temporariamente o controlo feed forward para testes.

--- SCDTR.ino
+++ SCDTR.ino
@@ -1,27 +1,63 @@
 #include <TimerOne.h>
 
-const int pinLed = 5;
-const int pinSensor = 1;
-double integrator = 0.0;
+const int pinLed[] = {3,5};
+const int pinSensor[] = {0,1};
+
+double integrator[] = {0, 0};
 const double timestep = 150000;
 
-struct params{
-  double k=0,theta=0;
-} Params;
+double K[2][2] = {{0,0},{0,0}};
+double theta[2] = {0,0};
 
+struct params{
+  double K, theta;
+};
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(pinLed, OUTPUT);
+  for(int i=0; i<2; i++){
+    pinMode(pinLed[i], OUTPUT);
+  }
   Serial.begin(9600); 
   Serial.println("Starting system identification");
-  Params = identificationRoutine(pinLed, pinSensor, 256, 15);
 
-  Serial.print("K = ");
-  Serial.print(Params.k,10);
+  struct params aux_params;
+
+  /*for(int i=0; i<2; i++){
+    for(int j=0; j<2; j++){
+      aux_params = identificationRoutine(pinLed[j], pinSensor[i], 256, 2); //Increase Number of samples maybe
+      K[i][j] = aux_params.K;
+      theta[i] = theta[i]+aux_params.theta;
+    }
+
+    theta[i] = theta[i]/2.0;
+  }*/
+   
+  Serial.print("K = {");
+  for(int i=0; i<2; i++){
+    for(int j=0; j<2; j++){
+        Serial.print(K[i][j],10);
+
+        if(i!=1 || j!=1){
+          Serial.print(", ");
+        }
+        else{
+          Serial.print("} ");
+        }
+          
+    }
+  }
   Serial.print(" || ");
   Serial.print("Theta = ");
-  Serial.println(Params.theta,10);
+  for(int i=0; i<2; i++){
+    Serial.println(theta[i],10);
+    if(i!=1){
+      Serial.print(", ");
+    }
+    else{
+      Serial.print("} ");
+    }    
+  }
 
   Timer1.initialize(timestep);
   Timer1.attachInterrupt(control);
@@ -61,48 +97,80 @@ params identificationRoutine(int pinLed, int pinSensor, int range, int samples){
     }
   }
 
+  analogWrite(pinLed,0);
+  
   det = N*Sv2 - Sv*Sv;
-  ret.k = (N*Svl - Sv*Sl)/det;
+  ret.K = (N*Svl - Sv*Sl)/det;
   ret.theta = (-Sv*Svl + Sv2*Sl)/det;
+  Serial.println("One fourth done");
   return ret;
 }
 
-int controller(double Lux, double LuxRef){
-  double FFwd = (1/Params.k)*(LuxRef-Params.theta);
-  double FBck = 0.2*(LuxRef-Lux);
-  
-  double ret = (FFwd+FBck+integrator);
-  
-  if( (ret < 255.0 || (LuxRef-Lux)<0) && (ret>0 || (LuxRef-Lux)>0)) {
-    integrator = integrator + 10.0*timestep/1000000.0*(LuxRef-Lux);
-  }
-  
-  ret = (FFwd+FBck+integrator);
+void controller(double Lux[], double LuxRef[], int v[]){
 
-  if(ret >= 255.0){
-    ret  = 255.0;
+  double det = K[0][0]*K[1][1] - K[0][1]*K[1][0];
+  
+  double FFwd[] = { K[0][0]*(LuxRef[0]-theta[0])/det - K[0][1]*(LuxRef[1]-theta[1])/det,
+                   -K[1][0]*(LuxRef[0]-theta[0])/det + K[1][1]*(LuxRef[1]-theta[1])/det};
+  
+  double FBck[] = { 0.2*(LuxRef[0]-theta[0]), 0.2*(LuxRef[1]-theta[1])};
+  
+  double ret[] = {0,0};
+
+  int actual_ret[] = {0,0};
+  
+  for(int i=0; i<2; i++){
+    ret[i] = /*FFwd[i] + */FBck[i] + integrator[i];
+
+    if( (ret[i] < 255.0 || (LuxRef[i]-Lux[i])<0) && (ret[i]>0 || (LuxRef[i]-Lux[i])>0)) {
+      integrator[i] = integrator[i] + timestep/1000000.0*(LuxRef[i]-Lux[i]);
+    }
+    
+    ret[i] = /*FFwd[i]*0 + */FBck[i] + integrator[i];
+  
+    if(ret[i] >= 255.0){
+      ret[i]  = 255.0;
+    }
+    
+    if(ret[i] <= 0){
+      ret[i] = 0;
+    }
+  
   }
+
   
-  if(ret <= 0){
-    ret = 0;
+  for(int i=0; i<2; i++){
+    Serial.print(i);
+    Serial.print(": FFwd = ");
+    Serial.print(FFwd[i]);
+    Serial.print(" || FBck = ");
+    Serial.print(FBck[i]);
+    Serial.print(" || Intg = ");
+    Serial.println(integrator[i]);
   }
+  Serial.println("");
   
-  Serial.print("FFwd = ");
-  Serial.print(FFwd);
-  Serial.print(" || FBck = ");
-  Serial.print(FBck);
-  Serial.print(" || Intg = ");
-  Serial.println(integrator);
-  
-  return round(ret);
+  for(int i=0; i<2; i++){
+    v[i] = round(ret[i]);
+  }
 }
 
 void control(){
-  double L = analogInput2Lux(analogRead(pinSensor));
-  int v = controller(L, 20);
-  analogWrite(pinLed,v);
+  double L[2], Lref[2]={20.0,20.0};
 
-  Serial.print("Lux = ");
-  Serial.println(L);
+  for(int i=0; i<2; i++){
+    L[i] = analogInput2Lux(analogRead(pinSensor[i]));
+  }
+    
+  int v[2];
+  controller(L, Lref, v);
+
+  for(int i=0; i<2; i++){
+    analogWrite(pinLed[i],v[i]);
+  
+    Serial.print(i);
+    Serial.print(": Lux = ");
+    Serial.println(L[i]);
+  }
 }
 
